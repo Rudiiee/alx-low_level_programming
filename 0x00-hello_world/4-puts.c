228409773c2fdac21248c4f6b064e8ffdc5f3352
Rudo Anita Nyamugama <rudoanyamugama@gmail.com>
2023-02-23 03:40:26 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,14 @@
 /**
  * main - prints exactly "Programming is like building a multilingual puzzle
  * followed by a new line
+ *
  * Return: 0
  */
 int main(void)
 {
 	char std[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(std);
-	return 0;
+
+	return (0);
 }
