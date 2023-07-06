250921cb9cb2c122fb235a9d4be7e29fc3f27e8f
root <rugoanyamugama@gmail.com>
2023-07-06 14:25:48 -0700

    0x08-recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/*
+ * _puts_recursion function to print a string
+ * followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *s);
+{
+	if(*s)
+	{
+		_putchar(*s)
+	}
+	else
+		_putchar('\n');
+}
