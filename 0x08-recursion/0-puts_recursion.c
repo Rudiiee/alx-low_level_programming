e8798ebb629c9a34a1b3172ef83402311bffcd13
Rudo <rugoanyamugama@gmail.com>
2023-07-06 14:57:58 -0700

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,7 +3,7 @@
 /**
  * _puts_recursion - function that prints a string
  * followed by a new line
- *
+ *@s: input
  * Return: Always 0 (Success)
  */
 void _puts_recursion(char *s)
@@ -11,7 +11,7 @@ void _puts_recursion(char *s)
 	if (*s)
 	{
 		_putchar(*s);
-		_puts_recursion(s+1);
+		_puts_recursion(s + 1);
 	}
 	else
 		_putchar('\n');
